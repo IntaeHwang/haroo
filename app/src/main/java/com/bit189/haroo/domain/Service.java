a27ec9e55ad345cb60c4174b8b91d12c632f023c
IntaeHwang <intae4708@naver.com>
2021-05-09 12:57:07 +0900

    210509-3

--- app/src/main/java/com/bit189/haroo/domain/Service.java
+++ app/src/main/java/com/bit189/haroo/domain/Service.java
@@ -1,5 +1,15 @@
 package com.bit189.haroo.domain;
 
-public class Service {
+import java.sql.Date;
+import java.sql.Time;
 
+public class Service {
+  private int no;
+  private String name;
+  private String intro;
+  private String coverImage;
+  private double averageRate;
+  private boolean state;
+  private Date registeredDate;
+  private Time registeredTime;
 }
