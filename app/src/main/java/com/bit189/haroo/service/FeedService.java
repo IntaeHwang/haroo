97c5b647427e41b2adafc9b777e48a2bef660c97
ssseongeunkim <ourment@naver.com>
2021-05-26 09:23:36 +0900

    210526_s1_feedAdd 수정완료

--- app/src/main/java/com/bit189/haroo/service/FeedService.java
+++ app/src/main/java/com/bit189/haroo/service/FeedService.java
@@ -1,12 +1,13 @@
 package com.bit189.haroo.service;
 
 import java.util.List;
+import com.bit189.haroo.domain.AttachedFile;
 import com.bit189.haroo.domain.Feed;
 import com.bit189.haroo.domain.Post;
 
 public interface FeedService {
 
-  int add(Post post, Feed feed) throws Exception;
+  int add(Post post, List<AttachedFile> files, Feed feed) throws Exception;
 
   List<Feed> list() throws Exception;
 
