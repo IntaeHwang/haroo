7860d9b0806bd2952a90d1505cb1b26b49bb4150
js988174 <76925402+js988174@users.noreply.github.com>
2021-05-31 16:51:58 +0900

    .

--- app/src/main/java/com/bit189/haroo/web/QuestionController.java
+++ app/src/main/java/com/bit189/haroo/web/QuestionController.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.bit189.haroo.domain.AttachedFile;
 import com.bit189.haroo.domain.Member;
@@ -22,37 +25,31 @@ import net.coobird.thumbnailator.name.Rename;
 
 
 @Controller
+@RequestMapping("/question/")
 public class QuestionController{
 
+  ServletContext sc;
   ServiceQuestionService serviceQuestionService;
 
-  public QuestionController(ServiceQuestionService serviceQuestionService) {
+  public QuestionController(ServiceQuestionService serviceQuestionService, ServletContext sc) {
     this.serviceQuestionService = serviceQuestionService;
+    this.sc = sc;
   }
 
-  @RequestMapping("/question/add")
-  public String execute(HttpServletRequest request, HttpServletResponse response)
+  @GetMapping("form")
+  public void form() throws Exception {
+  }
+
+  @RequestMapping("add")
+  public String add(Question question, Post post, HttpSession session)
       throws Exception {
 
-    String uploadDir = request.getServletContext().getRealPath("/upload");
+    String uploadDir = sc.getRealPath("/upload");
 
-    if (request.getMethod().equals("GET")) {
-      return "/jsp/serviceQuestion/form.jsp";
-    }
-
-
-    Post post = new Post();
-    Question question = new Question();
     ArrayList<AttachedFile> attachedFiles = new ArrayList<>();
 
-    question.setTitle(request.getParameter("title"));
-    question.setSecret(Integer.parseInt(request.getParameter("secret")));
-    post.setContent(request.getParameter("content"));
-
-
-    Member m = new Member();
-    m.setNo(2);
-    question.setWriter(m);
+    Member loginUser = (Member) session.getAttribute("loginUser");
+    question.setWriter(loginUser);
 
     ServiceInfo s = new ServiceInfo();
     s.setNo(2);
@@ -120,18 +117,16 @@ public class QuestionController{
 
   }    
 
-  @RequestMapping("/question/delete")
-  public String delete(HttpServletRequest request, HttpServletResponse response)
-      throws Exception {
-
-    int no = Integer.parseInt(request.getParameter("no"));
+  @GetMapping("delete")
+  public String delete(int no, HttpSession session) throws Exception {
 
     Question oldQuestion = serviceQuestionService.get(no);
+
     if (oldQuestion == null) {
       throw new Exception("해당 번호의 게시글이 없습니다.");
     }
 
-    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+    Member loginUser = (Member) session.getAttribute("loginUser");
     if (oldQuestion.getWriter().getNo() != loginUser.getNo()) {
       throw new Exception("삭제 권한이 없습니다!");
     }
@@ -142,128 +137,115 @@ public class QuestionController{
 
   }
 
-  @RequestMapping("/question/detail")
-  public String detail(HttpServletRequest request, HttpServletResponse response)
+  @GetMapping("detail")
+  public void detail(int no, Model model)
       throws Exception {
 
-
-    int no = Integer.parseInt(request.getParameter("no"));
-
     Question question = serviceQuestionService.get(no);
-    request.setAttribute("question", question);
-
-    return "/jsp/serviceQuestion/detail.jsp";
+    model.addAttribute("question", question);
 
   }
 
-  @RequestMapping("/question/list")
-  public String list(HttpServletRequest request, HttpServletResponse response)
+  @GetMapping("list")
+  public void list(Model model)
       throws Exception {
 
     List<Question> questions = serviceQuestionService.list();
 
-    request.setAttribute("questions", questions);
-
-    return "/jsp/serviceQuestion/list.jsp";
+    model.addAttribute("questions", questions);
 
   }    
 
-  @RequestMapping("/question/update")
-  public String update(HttpServletRequest request, HttpServletResponse response)
+  @PostMapping("update")
+  public String update(int no, Question question, HttpSession session)
       throws Exception {
 
-    int no = Integer.parseInt(request.getParameter("no"));
-
-    Question oldQuestion = serviceQuestionService.get(no);
+    Question oldQuestion = serviceQuestionService.get(question.getNo());
     if (oldQuestion == null) {
       throw new Exception("해당 번호의 게시글이 없습니다.");
     } 
 
-    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+    Member loginUser = (Member) session.getAttribute("loginUser");
     if (oldQuestion.getWriter().getNo() != loginUser.getNo()) {
       throw new Exception("변경 권한이 없습니다!");
     }
 
-    Question q = new Question();
-    q.setNo(oldQuestion.getNo());
-    q.setContent(request.getParameter("content"));
-    serviceQuestionService.update(q);
+    serviceQuestionService.update(question);
 
     return "redirect:list";
 
   }
 
-  @RequestMapping("/question/reply/add")
-  public String replyAdd(HttpServletRequest request, HttpServletResponse response)
-      throws Exception {
-
-    String uploadDir = request.getServletContext().getRealPath("/upload");
-
-
-    if (request.getMethod().equals("GET")) {
-      return "/jsp/serviceQuestion/form2.jsp";
-    }
-
-    int no = Integer.parseInt(request.getParameter("no"));
-
-    Question oldQuestion = serviceQuestionService.get(no);
-
-    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
-    if (oldQuestion.getWriter().getNo() != loginUser.getNo()) {
-      throw new Exception("작업 권한이 없습니다!");
-    }
-
-    Question question = new Question();
-    question.setNo(oldQuestion.getNo());
-    question.setReplyContent(request.getParameter("content"));
-
-
-    AttachedFile attachedFile = new AttachedFile();
-
-    Collection<Part> files = request.getParts();
-    for (Part file : files) {
-      if (file.getName().equals("file") && file.getSize() > 0) {
-        System.out.println(">" + file.getSubmittedFileName());
-
-        System.out.println("uploadDir1 : " + uploadDir);
-
-        if (file.getSize() > 0) {
-          String filename = UUID.randomUUID().toString();
-
-          System.out.println("uploadDir2 : " + uploadDir);
-
-          file.write(uploadDir + "/" + filename);
-          System.out.println("uploadDir3 : " + uploadDir);
-          System.out.println(uploadDir + "/");
-
-
-          attachedFile.setName(filename);
-
-          //attachedFile.add(attachedFile);
-
-          System.out.println("uploadDir4 : " + uploadDir);
-
-          Thumbnails.of(uploadDir + "/" + filename)
-          .size(500, 500)
-          .outputFormat("jpg")
-          .crop(Positions.CENTER)
-          .toFiles(new Rename() {
-            @Override
-            public String apply(String name, ThumbnailParameter param) {
-              return name + "_300x300";
-            }
-          });
-        }
-      }
-
-      System.out.println("uploadDir5 : " + uploadDir);
-    }
-
-    serviceQuestionService.replyUpdate(question, attachedFile);
-
-    return "redirect:list";
-
-
-  }    
+  //  @RequestMapping("reply/add")
+  //  public String replyAdd(@RequestParam(defaultValue = "0") int no, Question question, Model model)
+  //      throws Exception {
+  //
+  //    String uploadDir = request.getServletContext().getRealPath("/upload");
+  //
+  //    if (request.getMethod().equals("GET")) {
+  //      return "/jsp/serviceQuestion/form2.jsp";
+  //    }
+  //
+  //    int no = Integer.parseInt(request.getParameter("no"));
+  //
+  //    Question oldQuestion = serviceQuestionService.get(no);
+  //
+  //    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+  //    if (oldQuestion.getWriter().getNo() != loginUser.getNo()) {
+  //      throw new Exception("작업 권한이 없습니다!");
+  //    }
+  //
+  //    Question question = new Question();
+  //    question.setNo(oldQuestion.getNo());
+  //    question.setReplyContent(request.getParameter("content"));
+  //
+  //
+  //    AttachedFile attachedFile = new AttachedFile();
+  //
+  //    Collection<Part> files = request.getParts();
+  //    for (Part file : files) {
+  //      if (file.getName().equals("file") && file.getSize() > 0) {
+  //        System.out.println(">" + file.getSubmittedFileName());
+  //
+  //        System.out.println("uploadDir1 : " + uploadDir);
+  //
+  //        if (file.getSize() > 0) {
+  //          String filename = UUID.randomUUID().toString();
+  //
+  //          System.out.println("uploadDir2 : " + uploadDir);
+  //
+  //          file.write(uploadDir + "/" + filename);
+  //          System.out.println("uploadDir3 : " + uploadDir);
+  //          System.out.println(uploadDir + "/");
+  //
+  //
+  //          attachedFile.setName(filename);
+  //
+  //          //attachedFile.add(attachedFile);
+  //
+  //          System.out.println("uploadDir4 : " + uploadDir);
+  //
+  //          Thumbnails.of(uploadDir + "/" + filename)
+  //          .size(500, 500)
+  //          .outputFormat("jpg")
+  //          .crop(Positions.CENTER)
+  //          .toFiles(new Rename() {
+  //            @Override
+  //            public String apply(String name, ThumbnailParameter param) {
+  //              return name + "_300x300";
+  //            }
+  //          });
+  //        }
+  //      }
+  //
+  //      System.out.println("uploadDir5 : " + uploadDir);
+  //    }
+  //
+  //    serviceQuestionService.replyUpdate(question, attachedFile);
+  //
+  //    return "redirect:list";
+  //
+  //
+  //  }    
 }
 
